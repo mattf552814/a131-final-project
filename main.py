96bf737e12151e7c88cdc422dd8d8173707ca314
Matthew Fellenz <552814@my.cuhsd.org>
2020-12-10 12:40:49 -0800

    It should be white's turn first

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ win = turtle.Screen()
 board_turtle = turtle.Turtle()
 board_turtle.speed(10)
 
-is_blacks_turn = True  # false means it's white's turn
+is_blacks_turn = False  # false means it's white's turn
 
 FONT_SIZE = 20
 FONT = ('PT Sans', FONT_SIZE, 'normal')
